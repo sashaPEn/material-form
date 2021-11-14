2f83b9a45a5622b0ca43716cbc353c92efd7eb69
pestrige <pestrige@gmail.com>
2021-11-14 23:23:04 +0300

    task from Dmitry Korovin

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Grid, Paper } from '@mui/material';
+import Form from './components/Form/Form';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Grid container justifyContent='center' alignItems='center' minHeight='100vh'>
+      <Grid item xs={11} md={8}>
+        <Paper elevation={3}>
+          <Form />
+        </Paper>
+      </Grid>
+    </Grid>
   );
 }
 
